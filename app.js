47a067ba260c6bba29c2b3f0033744c62cff1d20
davidschlundtbodien <d.schlundtbodien@gmail.com>
2019-12-13 13:40:12 -0800

    adds action to rolldice to display dice png

--- app.js
+++ app.js
@@ -8,3 +8,28 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // Random Number
+  var dice = Math.floor(Math.random() * 6 ) + 1;
+
+  //Display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  // Update the total if roll was not a one
+});
